de709cc8bf0fffe13ede5fb801adc2db8469605f
Kevin Zhang <kevindotzhang@gmail.com>
2025-06-15 23:07:48 -0400

    added cors

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
 import numpy as np
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 with open("horse_model.pkl", "rb") as f:
     model = pickle.load(f)
